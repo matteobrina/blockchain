6637b5fa4989dc58efc91163cd93fa779fba0163
Matteo Brina <matteo.brina@edu.unife.it>
2022-11-08 13:09:07 +0100

    secondo commit

--- blockchain-project/src/main/java/unichain/Unichain.java
+++ blockchain-project/src/main/java/unichain/Unichain.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 public class Unichain {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty=5;
     
     public static void main(String[] args) {
         blockchain.add(new Block("Sono il PRIMO blocco", "0"));
@@ -23,6 +24,7 @@ System.out.println(blockchainJson);
 public static Boolean isChainValid() {
     Block currentBlock;
     Block previousBlock;
+    String hashTarget = new String(new char[difficulty]).replace('\0', '0');
     for(int i=1; i < blockchain.size(); i++) {
     currentBlock = blockchain.get(i);
     previousBlock = blockchain.get(i-1);
@@ -34,6 +36,11 @@ public static Boolean isChainValid() {
         System.out.println("Previous Hashes not equal");
         return false;
         }
+
+        if(!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+            System.out.println("This block hasn't been mined");
+            return false;
+            }
     }
     return true;
 }
